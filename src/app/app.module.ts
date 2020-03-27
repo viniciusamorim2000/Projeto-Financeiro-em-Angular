28dc797f9589e9743a61b13154745ca2869e7b6e
Vinicius <amorimhavoc@gmail.com>
2020-03-27 08:35:28 -0300

    Componente pessoas-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
 import { FormsModule } from '@angular/forms';
 import { MessageComponent } from './message/message.component';
 import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.co
     LancamentoCadastroComponent,
     PessoaCadastroComponent,
     MessageComponent,
-    LancamentosGridComponent
+    LancamentosGridComponent,
+    PessoasGridComponent
   ],
   imports: [
     BrowserModule,
